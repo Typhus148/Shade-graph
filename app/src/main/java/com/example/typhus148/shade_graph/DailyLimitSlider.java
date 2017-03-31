dfb54341033e90ca53d496fe2cd4b22fd406fe94
Typhus148 <prhciv@yahoo.com>
2017-03-31 16:32:10 -0400

    Added the code to allow for vertical seekbar functionality.

--- app/src/main/java/com/example/typhus148/shade_graph/DailyLimitSlider.java
+++ app/src/main/java/com/example/typhus148/shade_graph/DailyLimitSlider.java
@@ -1,8 +1,65 @@
 package com.example.typhus148.shade_graph;
 
-/**
- * Created by typhus148 on 3/30/17.
- */
 
-public class DailyLimitSlider {
+import android.content.Context;
+import android.graphics.Canvas;
+import android.support.v7.widget.AppCompatSeekBar;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+
+
+public class DailyLimitSlider extends AppCompatSeekBar {
+
+    public DailyLimitSlider(Context context) {
+        super(context);
+    }
+
+    public DailyLimitSlider(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    public DailyLimitSlider(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(h, w, oldh, oldw);
+    }
+
+    @Override
+    protected synchronized void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(heightMeasureSpec, widthMeasureSpec);
+        setMeasuredDimension(getMeasuredHeight(), getMeasuredWidth());
+    }
+
+    protected void onDraw(Canvas c) {
+        c.rotate(-90);
+        c.translate(-getHeight(),0);
+        super.onDraw(c);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (!isEnabled()) {
+            return false;
+        }
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_MOVE:
+            case MotionEvent.ACTION_UP:
+                int i=0;
+                i=getMax() - (int) (getMax() * event.getY() / getHeight());
+                setProgress(i);
+                // Log.i("Progress",getProgress()+"");
+                onSizeChanged(getWidth(), getHeight(), 0, 0);
+                break;
+
+            case MotionEvent.ACTION_CANCEL:
+                break;
+        }
+        return true;
+    }
+
 }
