d0504cf8959c9f611ad2aff671b51f7beb09cb78
Typhus148 <prhciv@yahoo.com>
2017-04-03 16:07:34 -0400

    Refactored code to comply with the ability to switch outputs and display based on which screen of the app it is on.

--- app/src/main/java/com/example/typhus148/shade_graph/BarTick.java
+++ app/src/main/java/com/example/typhus148/shade_graph/BarTick.java
@@ -1,6 +1,7 @@
 package com.example.typhus148.shade_graph;
 
 import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
@@ -8,7 +9,7 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class BarTick extends RelativeLayout {
-    public BarTick(Context context, boolean isToday, String xLabel, boolean overSized) {
+    public BarTick(Context context, boolean isToday, String xLabel, boolean overSized, int barColor, String appScreen) {
         super(context);
         LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         inflater.inflate(R.layout.layout_bar_tick, this, true);
@@ -17,18 +18,30 @@ public class BarTick extends RelativeLayout {
         View graphBar = findViewById(R.id.graphbar);
         View oversizedBar = findViewById(R.id.oversizedBar);
 
-        if (overSized) {
-            cutoffBlock.setVisibility(VISIBLE);
-            oversizedBar.setVisibility(VISIBLE);
-            graphBar.setVisibility(INVISIBLE);
-        }
+        // Sets the color of the graph bars
+        GradientDrawable graphBarDrawable = (GradientDrawable) graphBar.getBackground().getCurrent();
+        graphBarDrawable.setColor(barColor);
 
-        if (!isToday) {
-            TextView xlabel = (TextView)findViewById(R.id.graphXLabel);
-            xlabel.setText(xLabel);
-            todayMarker.setVisibility(INVISIBLE);
+        TextView xlabel = (TextView) findViewById(R.id.graphXLabel);
+
+        if (appScreen.equals("History")) {
+            if (overSized) {
+                cutoffBlock.setVisibility(VISIBLE);
+                oversizedBar.setVisibility(VISIBLE);
+                graphBar.setVisibility(INVISIBLE);
+            }
+
+            if (!isToday) {
+                xlabel.setText(xLabel);
+                todayMarker.setVisibility(INVISIBLE);
+            } else {
+                todayMarker.setVisibility(VISIBLE);
+            }
         } else {
-            todayMarker.setVisibility(VISIBLE);
+            cutoffBlock.setVisibility(INVISIBLE);
+            oversizedBar.setVisibility(INVISIBLE);
+            todayMarker.setVisibility(INVISIBLE);
+            xlabel.setText(xLabel);
         }
     }
 }
